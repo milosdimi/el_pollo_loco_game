d282ae73af5f7d432cd504463c82eed6fbaa7226
milosdimi <132084913+milosdimi@users.noreply.github.com>
2025-04-26 18:40:48 +0200

    ../img fixed

--- models/character.class.js
+++ models/character.class.js
@@ -1,7 +1,7 @@
 class Character extends MovableObject {
 
     constructor() {
-        super().loadImage("../img/2_character_pepe/2_walk/W-21.png");
+        super().loadImage("img/2_character_pepe/2_walk/W-21.png");
     }
 
 
