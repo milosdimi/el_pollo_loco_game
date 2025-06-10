a7d3dfcd00400699f7c7912cb64a5554814d35e5
milosdimi <132084913+milosdimi@users.noreply.github.com>
2025-06-10 12:46:17 +0200

    modul ende

--- levels/level1.js
+++ levels/level1.js
@@ -1,39 +1,44 @@
-const level1 = new Level(
-    [
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-        new Endboss()
-    ],
+let level1;
+function initLevel1() {
+    level1
 
-    [
-        new Cloud(),
-    ],
-    [
-        new BackgroundObject("img/5_background/layers/air.png", -719),
-        new BackgroundObject("img/5_background/layers/3_third_layer/2.png", -719),
-        new BackgroundObject("img/5_background/layers/2_second_layer/2.png", -719),
-        new BackgroundObject("img/5_background/layers/1_first_layer/2.png", -719),
+    level1 = new Level(
+        [
+            new Chicken(),
+            new Chicken(),
+            new Chicken(),
+            new Endboss()
+        ],
 
-        new BackgroundObject("img/5_background/layers/air.png", 0),
-        new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
-        new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
-        new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
-        new BackgroundObject("img/5_background/layers/air.png", 719),
-        new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719),
-        new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719),
-        new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719),
+        [
+            new Cloud(),
+        ],
+        [
+            new BackgroundObject("img/5_background/layers/air.png", -719),
+            new BackgroundObject("img/5_background/layers/3_third_layer/2.png", -719),
+            new BackgroundObject("img/5_background/layers/2_second_layer/2.png", -719),
+            new BackgroundObject("img/5_background/layers/1_first_layer/2.png", -719),
 
-        new BackgroundObject("img/5_background/layers/air.png", 719 * 2),
-        new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 719 * 2),
-        new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 719 * 2),
-        new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 719 * 2),
-        new BackgroundObject("img/5_background/layers/air.png", 719 * 3),
-        new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719 * 3),
-        new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719 * 3),
-        new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719 * 3),
+            new BackgroundObject("img/5_background/layers/air.png", 0),
+            new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 0),
+            new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 0),
+            new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 0),
+            new BackgroundObject("img/5_background/layers/air.png", 719),
+            new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719),
+            new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719),
+            new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719),
+
+            new BackgroundObject("img/5_background/layers/air.png", 719 * 2),
+            new BackgroundObject("img/5_background/layers/3_third_layer/1.png", 719 * 2),
+            new BackgroundObject("img/5_background/layers/2_second_layer/1.png", 719 * 2),
+            new BackgroundObject("img/5_background/layers/1_first_layer/1.png", 719 * 2),
+            new BackgroundObject("img/5_background/layers/air.png", 719 * 3),
+            new BackgroundObject("img/5_background/layers/3_third_layer/2.png", 719 * 3),
+            new BackgroundObject("img/5_background/layers/2_second_layer/2.png", 719 * 3),
+            new BackgroundObject("img/5_background/layers/1_first_layer/2.png", 719 * 3),
 
 
 
-    ]
-);
+        ]
+    );
+}
